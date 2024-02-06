92bcc6d348554b0d36f10cd3df6c451d5b78a3ea
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-02-06 02:17:29 +0000

    Update employee.js

--- src/components/employee.js
+++ src/components/employee.js
@@ -40,7 +40,7 @@ function Employee() {
   return (
     <div className='px-5 py-3'>
       <div className='d-flex justify-content-center mt-2'>
-        <h3>Employee List</h3>
+        <h3>.</h3>
       </div>
       <Link to="/create" className='btn btn-success'>Add Employee</Link>
       <div className='mt-3'>
